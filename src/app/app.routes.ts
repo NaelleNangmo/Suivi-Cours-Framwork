ad399646f7b4f639178c8706dc4dd80c4d7a4607
NaelleNangmo <naelle.noutong@homesolutionsworld.org>
2025-11-10 12:10:48 +0100

    Mise à jour du projet suivi-cours avec ajout des pages listing et inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Content } from './content/content';
+import { Inscription } from './inscription/inscription';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Content},
+    {path: "inscription", component: Inscription}
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Content } from './content/content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Header,
+    Footer,
+    Content,
+    RouterOutlet
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('suivi-cours');
+  protected readonly title = 'Ma prmiere donnee';
 }
